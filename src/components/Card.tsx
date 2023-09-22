943d10372d9325e79307f137862825e2da2c92cb
nikosetiawanp <nikosetiawanp@gmail.com>
2023-09-22 10:56:27 +0700

    docs: notification works, accordion works

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,4 +1,4 @@
-import { Disclosure } from "@headlessui/react";
+import { Disclosure, Transition } from "@headlessui/react";
 import IconChevronDown from "../assets/icon-chevron-down.svg";
 import { useState } from "react";
 
@@ -23,22 +23,16 @@ export default function Card(props: {
     try {
       const response = await deleteOffice(id);
       props.showNotification(response.message, 200);
-      console.log(response.code);
       props.setOffices((current) =>
         current?.filter((office) => office.id !== props.office.id)
       );
     } catch (error: any | unknown) {
-      console.log(error.name);
-
-      // props.setOffices((current) =>
-      //   current?.filter((office) => office.id !== props.office.id)
-      // );
       props.showNotification(error.message, 404);
     }
   };
 
   return (
-    <div className="shadow-lg w-full rounded-lg overflow-hidden bg-white">
+    <div className="shadow-lg w-full rounded-lg overflow-hidden bg-white h-fit">
       <Disclosure>
         {formIsActive ? (
           <EditForm setFormIsActive={setFormIsActive} office={props.office} />
@@ -48,7 +42,7 @@ export default function Card(props: {
               onClick={() => setIsOpen((current) => !current)}
               className={`${
                 isOpen ? "bg-primary-grey" : "bg-white"
-              } w-full flex justify-between items-center p-6 shadow-xl transition-all duration-500 z-20`}
+              } w-full flex justify-between items-center p-6 shadow-xl transition-all duration-500 z-50`}
             >
               <div className="text-left">
                 <h4
@@ -70,36 +64,46 @@ export default function Card(props: {
                 } transition-all duration-300`}
               />
             </Disclosure.Button>
-            <Disclosure.Panel className="flex flex-col gap-3 items-start p-6 bg-white">
-              <h5 className="text-primary-darkblue font-bold text-[20px]">
-                {props.office.detail.fullname}
-              </h5>
-              <p className="text-primary-darkblue text-[16px]">
-                {props.office.detail.job}
-              </p>
-              <p className="text-accent-blue text-[16px]">
-                {props.office.detail.email}
-              </p>
-              <p className="text-primary-darkblue text-[16px]">
-                {props.office.detail.phone}
-              </p>
-              <hr className="border-primary-lightgrey w-full" />
-              <div className="flex w-full justify-between items-center mt-2">
-                <button
-                  onClick={() => setFormIsActive(true)}
-                  className="text-[12px] text-primary-grey flex justify-between items-center gap-2"
-                >
-                  <img src={IconPencil} alt="icon-pencil" /> EDIT
-                </button>
-                {/* DELETE */}
-                <button
-                  onClick={() => removeOffice(`${props.office.id}`)}
-                  className="text-[12px] text-accent-red flex justify-between items-center gap-2"
-                >
-                  <img src={IconTrash} alt="icon-trash" /> DELETE
-                </button>
-              </div>
-            </Disclosure.Panel>
+
+            <Transition
+              enter="transition ease-in-out duration-300 transform"
+              enterFrom="-translate-y-full"
+              enterTo="translate-y-0"
+              leave="transition ease-in-out duration-300 transform"
+              leaveFrom="translate-y-0"
+              leaveTo="-translate-y-full"
+            >
+              <Disclosure.Panel className="flex flex-col gap-3 items-start p-6 bg-white">
+                <h5 className="text-primary-darkblue font-bold text-[20px]">
+                  {props.office.detail.fullname}
+                </h5>
+                <p className="text-primary-darkblue text-[16px]">
+                  {props.office.detail.job}
+                </p>
+                <p className="text-accent-blue text-[16px]">
+                  {props.office.detail.email}
+                </p>
+                <p className="text-primary-darkblue text-[16px]">
+                  {props.office.detail.phone}
+                </p>
+                <hr className="border-primary-lightgrey w-full" />
+                <div className="flex w-full justify-between items-center mt-2">
+                  <button
+                    onClick={() => setFormIsActive(true)}
+                    className="text-[12px] text-primary-grey flex justify-between items-center gap-2"
+                  >
+                    <img src={IconPencil} alt="icon-pencil" /> EDIT
+                  </button>
+                  {/* DELETE */}
+                  <button
+                    onClick={() => removeOffice(`${props.office.id}`)}
+                    className="text-[12px] text-accent-red flex justify-between items-center gap-2"
+                  >
+                    <img src={IconTrash} alt="icon-trash" /> DELETE
+                  </button>
+                </div>
+              </Disclosure.Panel>
+            </Transition>
           </>
         )}
       </Disclosure>
